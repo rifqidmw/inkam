52117628b5285b8dc883c61570b9c4b0018d6613
ahmadqorib <ahmadqorib97@gmail.com>
2018-11-30 13:49:37 +0700

    aaa

--- app/main.js
+++ app/main.js
@@ -1,6 +1,6 @@
 fetch('/inkam/data/info.json')
 .then(function(response) {
-  // Do stuff with the response
+  console.log('succes get data json');
 })
 .catch(function(error) {
   console.log('Looks like there was a problem: \n', error);
