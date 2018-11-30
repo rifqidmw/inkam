037bc207ea9ebb8fb0c7ba922a093e7668bad57d
ahmadqorib <ahmadqorib97@gmail.com>
2018-11-30 13:43:12 +0700

    aaa

--- app/main.js
+++ app/main.js
@@ -1,4 +1,7 @@
-if (!('fetch' in window)) {
-  console.log('Fetch API not found, try including the polyfill');
-  return;
-}
+fetch('examples/example.json')
+.then(function(response) {
+  // Do stuff with the response
+})
+.catch(function(error) {
+  console.log('Looks like there was a problem: \n', error);
+});
