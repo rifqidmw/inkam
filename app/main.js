aa6e545ad42d2a860fa861da0037e71232277f75
ahmadqorib <ahmadqorib97@gmail.com>
2018-11-30 14:14:17 +0700

    coba ada data json

--- app/main.js
+++ app/main.js
@@ -1,8 +1,12 @@
+var myList = document.getElementById("list");
 fetch('/inkam/data/info.json')
 .then(function(response) {
   return response.json();
 }).then(function(data) {
-	console.log(data);
+	for (var i = 0; i < data.data.length; i++) {
+      	var listItem = '<h5>'+data.data[i].info_name+'</h5>';
+      	myList.appendChild(listItem);
+	}
 })
 .catch(function(error) {
   console.log('Looks like there was a problem: \n', error);
