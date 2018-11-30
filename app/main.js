5943668279f8d4c9ffe7655de5360b8969c3d11e
ahmadqorib <ahmadqorib97@gmail.com>
2018-11-30 14:15:41 +0700

    coba meneh

--- app/main.js
+++ app/main.js
@@ -4,7 +4,7 @@ fetch('/inkam/data/info.json')
   return response.json();
 }).then(function(data) {
 	for (var i = 0; i < data.data.length; i++) {
-      	var listItem = '<h5>'+data.data[i].info_name+'</h5>';
+      	var listItem = '<h5>'+data.data[i].info_name+'coba</h5>';
       	myList.appendChild(listItem);
 	}
 })
