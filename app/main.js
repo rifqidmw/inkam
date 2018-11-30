387adc5c08ac3f079327dee63970229e159e152a
ahmadqorib <ahmadqorib97@gmail.com>
2018-11-30 14:57:10 +0700

    wikwikwik

--- app/main.js
+++ app/main.js
@@ -16,7 +16,7 @@ fetch('/inkam/data/info.json')
     	// div.appendChild(h5);
 
     	// myList.appendChild(div);
-    	var lay = '<div class="col-sm-6 col-12"><div class="card"><div class="card-body"><h5>'+data.data[i].info_name+'</h5></div></div></div>';
+    	var lay = '<div class="col-sm-6 col-12"><div class="card"><img class="card-img-top" src="/inkam/asset/'+data.data[i].info_image+'"><div class="card-body"><h5>'+data.data[i].info_name+'</h5><span class="tgl">'+data.data[i].created_at+'</span><p class="card-text">'+data.data[i].info_desc+'</p><a href="" class="btn btn-sm btn-outline-warning">Selengkapnya...</a></div></div></div>';
 		myList.insertAdjacentHTML('beforeend', lay);
 	}
 })
